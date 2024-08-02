ddaa66371b99f782944bb3a55daef2c737ccf561
Kooreis <kooreis.official@gmail.com>
2024-08-02 04:52:50 -0500

    Initial setup of the program with the Main method.

--- solution.cs
+++ solution.cs
@@ -1,21 +1,8 @@
-```C#
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Enter a number: ");
-        int number = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Number of trailing zeroes in " + number + "! is " + FindTrailingZeros(number));
-    }
-
-    static int FindTrailingZeros(int number)
-    {
-        int count = 0;
-        for (int i = 5; number / i >= 1; i *= 5)
-            count += number / i;
-        return count;
     }
 }
-```
