c9b08852c4ee3a6f7998638593912a3e80c79249
Kooreis <kooreis.official@gmail.com>
2024-08-02 04:52:50 -0500

    This update adds a function to find the number of trailing zeros in the factorial of a given number. It initializes a counter and a variable i to 5. It then enters a while loop that continues as long as n divided by i is greater than or equal to 1. Inside the loop, it adds the integer division of n by i to the counter and multiplies i by 5. Finally, it returns the counter.

--- solution.py
+++ solution.py
@@ -5,10 +5,3 @@ def find_trailing_zeros(n):
         count += int(n / i)
         i *= 5
     return int(count)
-
-def main():
-    num = int(input("Enter a number: "))
-    print("Number of trailing zeros in", num, "factorial is", find_trailing_zeros(num))
-
-if __name__ == "__main__":
-    main()
