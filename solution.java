b886a083f38f402a5eaf76066dba9fbde271702b
Kooreis <kooreis.official@gmail.com>
2024-08-02 04:52:50 -0500

    Imported the Scanner class from the java.util package to read user input.

--- solution.java
+++ solution.java
@@ -1,19 +1,1 @@
-```java
 import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a number:");
-        int number = scanner.nextInt();
-        System.out.println("Number of trailing zeroes in " + number + "! is " + findTrailingZeros(number));
-    }
-
-    static int findTrailingZeros(int n) {
-        int count = 0;
-        for (int i = 5; n / i >= 1; i *= 5)
-            count += n / i;
-        return count;
-    }
-}
-```
